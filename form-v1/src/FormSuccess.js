7d1aff4bdcd9affafe3d2605bd1d8f1a80cf603a
g-veronika <g.veronika2021@gmail.com>
2021-12-10 18:57:21 +0100

    adding css

---
+++ form-v1/src/FormSuccess.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FormSuccess = () => {
+    return (
+        <div className="form-content-right">
+            <div className="form-success">We have received your request!</div>
+            <img src="img/img-3.svg" alt="success-image" className="form-img-2" />
+        </div>
+    )
+}
+
+export default FormSuccess
